b0f86b22acdcfe451d0b7d61d475b480e28cfe26
shivacharan1015-code <shivacharantech1015@protonmail.com>
2024-01-05 16:33:56 +0530

    User Interface

--- SVG/ViewController.swift
+++ SVG/ViewController.swift
@@ -8,13 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let generateButton: UIButton = {
+       
+        let button = UIButton(configuration: .filled())
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Generate Dogs", for: .normal)
+        return button
+    }()
+    
+    private let recentButton: UIButton = {
+       
+        let button = UIButton(configuration: .filled())
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Recently Generated Dogs", for: .normal)
+        return button
+    }()
+    
+    private let introLabel: UILabel = {
+       
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Random Dog Generator!"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .title1)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(introLabel)
+        
+        view.addSubview(recentButton)
+        recentButton.addAction(UIAction(handler: { _ in
+            self.goToRecentlySavedScreen()
+        }), for: .touchUpInside)
+        
+        view.addSubview(generateButton)
+        generateButton.addAction(UIAction(handler: { _ in
+            self.goToGenerateDogsScreen()
+        }), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        NSLayoutConstraint.activate([
+            
+            introLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            introLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+        
+            recentButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            recentButton.heightAnchor.constraint(equalToConstant: 50),
+            recentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            generateButton.bottomAnchor.constraint(equalTo: recentButton.topAnchor, constant: -20),
+            generateButton.heightAnchor.constraint(equalToConstant: 50),
+            generateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 
+    func goToGenerateDogsScreen() {
+        
+        self.navigationController?.pushViewController(GenerateNewDogsViewController(), animated: true)
+    }
+    
+    func goToRecentlySavedScreen() {
+        
+        self.navigationController?.pushViewController(RecentlySavedViewController(), animated: true)
+    }
 
 }
 
