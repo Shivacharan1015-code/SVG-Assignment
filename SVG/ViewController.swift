5cdc47a15974bfb37648bb92625ecee105638827
shivacharan1015-code <shivacharantech1015@protonmail.com>
2024-01-05 16:09:18 +0530

    Initial commit

---
+++ SVG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SVG
+//
+//  Created by Shivacharan Reddy on 05/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
